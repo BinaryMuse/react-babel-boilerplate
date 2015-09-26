cd975670cb9091451fd63902c01c36dff4565a50
BinaryMuse <brandon@brandontilley.com>
2015-09-26 11:02:37 -0700

    :bug: Change setup script so package.json doesn't infer from starter repo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
-npm init -y
+npm init -y > /dev/null
 npm install --save babel babel-loader babel-runtime babel-core core-decorators webpack webpack-dev-server react@0.14.0-rc1 react-dom@0.14.0-rc1
 npm install --save-dev babel-plugin-react-transform react-transform-catch-errors react-transform-hmr redbox-react
-jq '.scripts.start = "babel-node ./node_modules/.bin/webpack-dev-server --inline --hot" | .private = true' package.json > modified-package.json
+jq '.scripts.start = "babel-node ./node_modules/.bin/webpack-dev-server --inline --hot" | .description = "" | .license = "MIT" | .private = true | del(.main, .repository, .bugs, .homepage)' package.json > modified-package.json
 mv modified-package.json package.json
