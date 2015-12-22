a179b6495c0c1e9a31683c8014a8bf2a995886f1
Michelle Tilley <michelle@michelletilley.net>
2015-12-22 10:07:00 -0800

    Update setup for Electron apps

--- setup/electron/install.sh
+++ setup/electron/install.sh
@@ -2,11 +2,13 @@ read -p "This will DESTRUCTIVELY convert this folder into a starter Electron app
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  npm uninstall --save webpack express webpack-dev-middleware webpack-hot-middleware
   npm install --save-dev electron-prebuilt electron-rebuild
   node ./setup/electron/modify-pkg.js
   # ./node_modules/.bin/electron-rebuild
   rm -r client
   rm -r public
+  rm -r server
   rm webpack.config.js
   rm webpack.config.es6.js
   cp -r ./setup/electron/template ./app
