55cf75c17fd4775d9d1343e5661e1d8fcfae5066
Michelle Tilley <michelle@michelletilley.net>
2015-12-23 16:19:10 -0800

    babel-plugin-react-transform@latest requires Babel 6

--- setup/install.sh
+++ setup/install.sh
@@ -1,7 +1,7 @@
 npm init -y > /dev/null
 node ./setup/modify-pkg.js
 npm install --save babel@^5 babel-loader@^5 babel-runtime@^5 babel-core@^5 core-decorators webpack react react-dom express webpack-dev-middleware webpack-hot-middleware
-npm install --save-dev babel-plugin-react-transform react-transform-catch-errors react-transform-hmr redbox-react
+npm install --save-dev babel-plugin-react-transform@^1 react-transform-catch-errors react-transform-hmr redbox-react
 cp -r ./setup/template/client ./client
 cp -r ./setup/template/public ./public
 cp -r ./setup/template/server ./server
